c806023c1a3f87f100ad7cf7196203b0f0e320f8
ghifar1327 <givartorreto@gmail.com>
2026-04-07 20:49:32 +0700

    feat(handler): add user authentication handlers for register and login

---
+++ backend/internals/handlers/user.go
@@ -0,0 +1,112 @@
+package handlers
+
+import (
+	"net/http"
+	"shortLink-app/internals/dto"
+	"shortLink-app/internals/services"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+type UserHandler struct {
+	service *services.UserServices
+}
+
+func NewUserHandler(s *services.UserServices) *UserHandler {
+	return &UserHandler{service: s}
+}
+
+// ==================================================================== register
+
+// Register godoc
+// @Summary Register new user
+// @Description Create a new user account
+// @Tags Authentication
+// @Accept json
+// @Produce json
+// @Param user body dto.RegisterRequest true "Register Request"
+// @Success 200 {object} dto.Response
+// @Failure 400 {object} dto.Response
+// @Router /auth/register [post]
+func (h *UserHandler) Register(ctx *gin.Context) {
+	var req dto.RegisterRequest
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, dto.Response{
+			Success: false,
+			Message: "Invalid request body",
+		})
+		return
+	}
+
+	err := h.service.Register(ctx, req)
+	if err != nil {
+
+		if pgErr, ok := err.(*pgconn.PgError); ok {
+			if pgErr.Code == "23505" {
+				ctx.JSON(401, dto.Response{
+					Success: false,
+					Message: "Email already registered",
+				})
+				return
+			}
+		}
+		ctx.JSON(http.StatusBadRequest, dto.Response{
+			Success: false,
+			Message: "Internal server error",
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, dto.Response{
+		Success: true,
+		Message: "User registered successfully",
+	})
+}
+
+// Login godoc
+// @Summary Login new user
+// @Description Create a new user account
+// @Tags Authentication
+// @Accept json
+// @Produce json
+// @Param user body dto.LoginRequest true "Login Request"
+// @Success 200 {object} dto.Response
+// @Failure 400 {object} dto.Response
+// @Router /auth/login [post]
+func (h *UserHandler) Login(ctx *gin.Context) {
+	var req dto.LoginRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, dto.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	user, err := h.service.GetUserBYEmail(ctx.Request.Context(), req.Email)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, dto.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+	token, err := h.service.Login(ctx.Request.Context(), req)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, dto.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, dto.Response{
+		Success: true,
+		Message: "Login Success",
+		Results: dto.LoginResponse{
+			Email:  user.Email,
+			UserID: user.ID,
+			Token:  token,
+		},
+	})
+}
