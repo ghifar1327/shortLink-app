8cf46be37ce6451ed0b4a0cf5c6ab6b67292b908
ghifar1327 <givartorreto@gmail.com>
2026-04-07 16:52:38 +0700

    feat: Register the main layout and its children to the router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import { createBrowserRouter } from 'react-router';
-import AuthLayout from './layout/AuthLayout';
+import { createBrowserRouter } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import { RouterProvider } from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
+import MainLayout from './layouts/MainLayout';
+import LandingPage from './pages/LandingPage';
+import AuthLayout from './layouts/AuthLayout';
+import Dashboard from './pages/Dashboard';
+import CreateLink from './pages/CreateLink';
 
 export default function App() {
   return (
@@ -14,6 +18,15 @@ export default function App() {
 
 function Router() {
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<MainLayout/>,
+      children: [
+        { index: true, element: <LandingPage/>},
+        {path: "dashboard", element: <Dashboard/>},
+        {path: "create-link",element: <CreateLink/>}
+      ]
+    },
     {
       element: <AuthLayout />,
       children: [
