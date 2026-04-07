7c85d2e15d6bee1c9bcad8c3eda54b419c5a467c
ghifar1327 <givartorreto@gmail.com>
2026-04-07 22:43:40 +0700

    feat: Add LinkRepository with create, fetch by user, and soft delete functionality

---
+++ backend/internals/repositories/link.go
@@ -0,0 +1,65 @@
+package repositories
+
+import (
+	"context"
+	"shortLink-app/internals/dto"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type LinkRepository struct {
+	db *pgxpool.Pool
+}
+
+func NewLinkRepository(db *pgxpool.Pool) *LinkRepository {
+	return &LinkRepository{db: db}
+}
+
+func (r *LinkRepository) CreateLink(ctx context.Context, link dto.CreateLinkRequest) error {
+	query := `
+	INSERT INTO links (user_id, original_url, slug, created_at)
+	VALUES ($1,$2,$3,$4)
+	`
+	_, err := r.db.Exec(ctx, query,
+		link.UserID,
+		link.OriginalURL,
+		link.Slug,
+		time.Now(),
+	)
+	return err
+}
+
+func (r *LinkRepository) GetAllLinkByUserID(ctx context.Context, userId int) ([]dto.LinkResponse, error) {
+	query := `
+	SELECT id, original_url, slug, created_at
+	FROM links
+	WHERE user_id = $1 AND deleted_at IS NULL
+	`
+	rows, err := r.db.Query(ctx, query, userId)
+	if err != nil {
+		return nil, err
+	}
+
+	link, err := pgx.CollectRows(rows, pgx.RowToStructByName[dto.LinkResponse])
+	if err != nil {
+		return nil, err
+	}
+	return link, nil
+}
+
+func (r *LinkRepository) SoftDeleteLink(ctx context.Context, id int) ([]dto.LinkResponse, error) {
+	var userId int
+	err := r.db.QueryRow(ctx, "SELECT user_id FROM links WHERE id=$1", id).Scan(&userId)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = r.db.Exec(ctx, "UPDATE links SET deleted_at=$1 WHERE id=$2", time.Now(), id)
+	if err != nil {
+		return nil, err
+	}
+
+	return r.GetAllLinkByUserID(ctx, userId)
+}
