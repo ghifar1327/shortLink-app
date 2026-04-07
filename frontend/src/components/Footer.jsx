1d45239c89b40fa971abf65f272bd27ed0e7a481
ghifar1327 <givartorreto@gmail.com>
2026-04-07 15:33:34 +0700

    feat: add footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='flex px-[5%] w-full p-10 justify-between text-2xl text-gray-500 bg-[#F3F4F5]'>
+        <p>© 2024 ShortLink. The Digital Architect.</p>
+        <p>Privacy Policy</p>
+        <p>Terms of Service</p>
+        <p>API Documentation</p>
+        <p>Support</p>
+    </footer>
+  )
+}
