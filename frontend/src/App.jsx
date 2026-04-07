04fe664055ecba3b45c07513a3be57f49759fc53
ghifar1327 <givartorreto@gmail.com>
2026-04-07 11:44:34 +0700

    feat: configure createBrowserRouter with AuthLayout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { createBrowserRouter } from 'react-router';
+import AuthLayout from './layout/AuthLayout';
+import LoginPage from './pages/LoginPage';
+import { RouterProvider } from 'react-router-dom';
+import RegisterPage from './pages/RegisterPage';
 
 export default function App() {
   return (
-    <div>App</div>
+    <Router></Router>
   )
+}
+
+
+function Router() {
+  const router = createBrowserRouter([
+    {
+      element: <AuthLayout />,
+      children: [
+        { path: "login", element: <LoginPage /> },
+        { path: "register", element: <RegisterPage /> },
+      ],
+    },    
+  ]);
+
+  return <RouterProvider router={router} />;
 }
