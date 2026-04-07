3312d387873627f9d03022b434b26e56001841a5
ghifar1327 <givartorreto@gmail.com>
2026-04-08 01:06:11 +0700

    feat: add auth meddleware

---
+++ backend/internals/middleware/authMiddleware.go
@@ -0,0 +1,55 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"shortLink-app/internals/dto"
+	"shortLink-app/internals/utils"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		authHeader := ctx.GetHeader("Authorization")
+
+		if authHeader == "" {
+			ctx.JSON(http.StatusUnauthorized, dto.Response{
+				Success: false,
+				Message: "Authorization header required",
+			})
+			ctx.Abort()
+			return
+		}
+
+		if !strings.HasPrefix(authHeader, "Bearer ") {
+			ctx.JSON(http.StatusUnauthorized, dto.Response{
+				Success: false,
+				Message: "Invalid token format",
+			})
+			ctx.Abort()
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		log.Println("AUTH HEADER:", authHeader)
+		log.Println("TOKEN:", tokenString)
+		
+		claims, err := utils.VerifyToken(tokenString)
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, dto.Response{
+				Success: false,
+				Message: "Invalid or expired token",
+			})
+			ctx.Abort()
+			return
+		}
+
+		ctx.Set("user_id", claims.Id)
+
+		ctx.Next()
+	}
+}
