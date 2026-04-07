7d782a849950d0c57684402689311dfc7f0995a7
ghifar1327 <givartorreto@gmail.com>
2026-04-08 04:05:30 +0700

    feat: implement create link page with form handling and LinkContext integration for generating short URLs

--- frontend/src/pages/CreateLink.jsx
+++ frontend/src/pages/CreateLink.jsx
@@ -1,38 +1,56 @@
 import { AiOutlineEye } from "react-icons/ai"; 
 import { ArrowLeft } from 'lucide-react'
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router'
 import Input from '../components/common/Input'
 import { Button } from "../components/common/Button";
+import LinkContext from "../context/LinkContex";
 
 export default function CreateLink() {
+  const [form, setForm] = React.useState({
+    original_url: "",
+    slug: "",
+  }); 
+  const {createLink} = useContext(LinkContext);
+  const handleChange = (e) => {
+    const { name, value } = e.target; 
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: value
+    }));
+
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createLink(form);
+  }   
   return (
         <main className='p-[5%] mt-[3%] px-[25%] flex-1 bg-[#F3F4F5] flex flex-col gap-5'>
           <Link to="/dashboard" className='text-primary flex gap-3 text-2xl items-center'><ArrowLeft/> Back to Dashboard</Link>    
           <h1 className='text-4xl font-bold'>Created New Short Link</h1>
           <p className='text-gray-700'>Transform your long URLs into clean, manageable assets.</p>
-          <section className='bg-white p-10 rounded-2xl flex flex-col gap-5'>
+          <form onSubmit={handleSubmit} className='bg-white p-10 rounded-2xl flex flex-col gap-5'>
             <div>
-             <Input placeholder="https://example.com/your-long-url-here" gray>Destination URL <span className='text-red-500'>*</span></Input>
+             <Input id={"original_url"} name="original_url" onChange={handleChange} placeholder="https://example.com/your-long-url-here" gray>Destination URL <span className='text-red-500'>*</span></Input>
              <p className='text-gray-400 text-sm mt-2'><i>Ensure your URL starts with http:// or https://</i></p>
             </div>
             <div>
-                <label htmlFor="">Custom Slug (Optional)</label>
+                <label htmlFor="slug">Custom Slug (Optional)</label>
                 <div className='flex items-center border rounded-l border-gray-300 mt-2'>
                  <p className='bg-gray-300 p-2 rounded-l '>short.link/</p>
-                 <input placeholder="my-custom-slug" className='flex-1 outline-none p-2 px-3'/>
+                 <input id="slug" name="slug" onChange={handleChange}  placeholder="my-custom-slug" className='flex-1 outline-none p-2 px-3'/>
                 </div>
              <p className='text-gray-400 text-sm mt-2'><i>Leave blank to generate a random unique identifier.</i></p>
             </div>
             <div className="flex flex-col p-5 text-primary bg-[#B4C5FF80]/50 rounded-xl border border-primary">
                 <p className="flex items-center gap-3"><AiOutlineEye /> Live Preview</p>
-                <p className="pl-6"><span className="text-black">Your short link will be:</span> https://short.link/my-custom-slug</p>
+                <p className="pl-6"><span className="text-black">Your short link will be: </span>{form.slug ? `https://short.link/${form.slug}` : 'https://short.link/your-custom-slug'}</p>
             </div>
             <div className=" flex items-center gap-2 mt-4">
-                <Button size="p-2 px-3" blue>Create Link</Button>
-                <Button size="p-2 px-3">Cancel</Button>
+                <Button type="submit" size="p-2 px-3" blue>Create Link</Button>
+                <Button type="reset" size="p-2 px-3">Cancel</Button>
             </div>
-          </section>
+          </form>
         </main>
   )
 }
