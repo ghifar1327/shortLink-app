da14f4655fb0270dfe10a65759d428c29ac4e106
ghifar1327 <givartorreto@gmail.com>
2026-04-08 11:44:26 +0700

    feat: add delete and copy link handler

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -8,13 +8,26 @@ import { AiOutlineSearch } from "react-icons/ai";
 import React, { useContext } from 'react'
 import Input from '../components/common/Input'
 import LinkContext from "../context/LinkContex";
+import { useNavigate } from "react-router";
 
 export default function Dashboard() {
-  const{fetchLinks, links } = useContext(LinkContext); 
+  const{fetchLinks, links ,deleteLink} = useContext(LinkContext); 
+  const navigate = useNavigate();
   React.useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/login", { replace: true });
+    }
     fetchLinks();
   }, []); 
-  console.log(links);
+
+  const handleCopy = async (url) => {
+  try {
+    await navigator.clipboard.writeText(url);
+  } catch (err) {
+    return err
+  }
+};
   return (
     <main className='p-[5%] mt-[3%] px-[25%] flex-1 bg-[#F3F4F5] flex flex-col gap-10'>
         <section className='w-full flex justify-between'>
@@ -32,15 +45,15 @@ export default function Dashboard() {
            <input className="flex-1 h-8 outline-none"/>
            <BsFilter size={33} />
         </section>
-        <section>
+        <section className="flex flex-col gap-5">
           {links.map((link) => (
              <div key={link.id} className="bg-white p-5 w-full rounded-xl flex justify-between items-center">
               <div className=" flex flex-col gap-3 w-[70%]">
-                <p className="text-primary font-bold flex items-center gap-3">
+                <div onClick={()=> { window.location.href = link.original_url;}} className="text-primary font-bold flex items-center cursor-pointer hover:text-blue-900 gap-3">
                   <BsLink size={30}/>
                   <span className="">{link.slug}</span>
-                </p>
-                <p className="line-clamp-1 w-full">{link.original_url}</p>
+                </div>
+                <p  className="line-clamp-1 w-full">{link.original_url}</p>
                 <div className="flex items-center gap-5 text-gray-500 font-bold">
                   <p className="flex items-center gap-2">
                     <BiCalendar size={20}/>
@@ -54,8 +67,10 @@ export default function Dashboard() {
                 </div>  
               </div>
               <div className="flex gap-3">
-                  <button className="text-blue-900 bg-purple-100 rounded-md p-2"><MdOutlineContentCopy size={25}/></button>
-                  <button className="text-gray-500"><FaRegTrashAlt size={25}/></button>
+                  <button className="cursor-pointer text-blue-900 bg-purple-100 rounded-md p-2" onClick={() => handleCopy(link.original_url)}><MdOutlineContentCopy size={25}/></button>
+                  <button className="cursor-pointer text-gray-500" onClick={() => deleteLink(link.id)}>
+                    <FaRegTrashAlt size={25}/>
+                  </button>
               </div>
           </div>
           ))}
