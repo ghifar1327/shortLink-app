1c810c5461e3fdfd5fb2dc6aecabf078757ea274
ghifar1327 <givartorreto@gmail.com>
2026-04-08 11:37:39 +0700

    feat: add generateslug for default slug

---
+++ backend/internals/utils/generateSlug.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"math/rand"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func GenerateSlug(length int) string {
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
