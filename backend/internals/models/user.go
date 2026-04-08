8cd6101599176984a4e417c1e6da241dc10ea32d
ghifar1327 <givartorreto@gmail.com>
2026-04-08 22:51:31 +0700

    feat: add field name and picture

--- backend/internals/models/user.go
+++ backend/internals/models/user.go
@@ -4,7 +4,9 @@ import "time"
 
 type User struct {
 	ID           int       `json:"id" db:"id"`
+	Picture      *string   `json:"picture" db:"picture"`
+	Name         string    `json:"name" db:"name"`
 	Email        string    `json:"email" db:"email"`
-	PasswordHash string    `json:"-" db:"password_hash"`
+	PasswordHash string    `json:"-" db:"-"`
 	CreatedAt    time.Time `json:"created_at" db:"created_at"`
 }
